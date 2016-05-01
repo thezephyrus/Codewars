c2b9c23234aceba75b5cc72d4af54e70bde49aaa
thezephyrus <neyo.napster@gmail.com>
2016-05-01 19:46:54 +0530

    Even or Odd
    http://www.codewars.com/kata/53da3dbb4a5168369a0000fe

---
+++ Java/EvenOrOdd.java
@@ -0,0 +1,10 @@
+/**
+Description:
+Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+*/
+
+public class EvenOrOdd {
+    public static String even_or_odd(int number) {
+        return number%2==0?"Even":"Odd";
+    }
+}
