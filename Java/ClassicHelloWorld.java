e907624981c7bc85aa4a8f88570f84ee7c7c67e9
thezephyrus <neyo.napster@gmail.com>
2016-05-01 20:24:51 +0530

    Classic Hello World
    http://www.codewars.com/kata/57036f007fd72e3b77000023/train/java

---
+++ Java/ClassicHelloWorld.java
@@ -0,0 +1,15 @@
+/**
+Description:
+The main mode is having a method named main inside a class and should return nothing but should 
+print a line to the standard output. For Java the main method should receive String array as parameters 
+that can be specified when running from console with the command
+java Solution.class parameter1 parameter2
+In javascript you can run it as
+Solution.main("parameter1","parameter2");
+*/
+
+public class Solution {
+  public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
