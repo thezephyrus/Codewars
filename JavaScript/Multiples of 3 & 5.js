7411188769a3be80155f022c91578969eb3c9743
thezephyrus <neyo.napster@gmail.com>
2016-05-03 21:45:32 +0530

    Sum Multiples of 3 or 5
    http://www.codewars.com/kata/multiples-of-3-and-5/javascript

---
+++ JavaScript/Multiples of 3 & 5.js
@@ -0,0 +1,15 @@
+/**
+	Description:
+	If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+	Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in.
+*/
+
+function solution(number){
+  var sum=0;
+  for(i=3;i<number;i++){
+    if(i%3==0||i%5==0){
+      sum+=i;
+    }
+  }
+  return sum;
+}
