403bb0e4ce7a1aab920289f5a24bf0f0ed6aaab4
thezephyrus <neyo.napster@gmail.com>
2016-05-03 21:57:32 +0530

    Triangular Treasure
    http://www.codewars.com/kata/525e5a1cb735154b320002c8

---
+++ Java/TriangularTreasure.java
@@ -0,0 +1,26 @@
+/**
+Description:
+Triangular numbers are so called because of the equilateral triangular shape that they occupy when laid out as dots. i.e.
+1st (1)   2nd (3)    3rd (6)
+*          **        ***
+           *         **
+                     *
+You need to return the nth triangular number. You should return 0 for out of range values:
+*/
+
+package org.vinil.sevenKyu;
+
+public class TriangularTreasure {
+
+	public static void main(String[] args) {
+
+		int num = 65;
+		int triangularNum = triangular(num);
+		System.out.println(triangularNum);
+	}
+
+	public static int triangular(int n) {
+		return n > 0 ? n * (n + 1) / 2 : 0;
+	}
+
+}
