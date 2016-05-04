33fbf49d95712ada1939c9124eb5d643c9bc5e6a
thezephyrus <neyo.napster@gmail.com>
2016-05-04 23:31:26 +0530

    set 2

---
+++ JavaScript/Regular Ball Super Ball-constructor overloading.js
@@ -0,0 +1,23 @@
+/**
+Description:
+Regular Ball Super Ball
+Create a class Ball.
+Ball objects should accept one argument for "ball type" when instantiated.
+
+If no arguments are given, ball objects should instantiate with a "ball type" of "regular."
+
+ball1 = new Ball();
+ball2 = new Ball("super");
+
+ball1.ballType     //=> "regular"
+ball2.ballType     //=> "super"
+*/
+
+var Ball = function(ballType) {
+   if(ballType === undefined){
+     this.ballType = "regular";
+   }
+  else{
+    this.ballType = ballType;
+  }
+};
