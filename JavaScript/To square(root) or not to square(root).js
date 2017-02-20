7280ba03ef0b81db1ea72ffc24ab0b8a1fb535c9
Vinil Pillai <thezephyrus@users.noreply.github.com>
2017-02-20 23:42:32 +1100

    Update To square(root) or not to square(root).js

--- JavaScript/To square(root) or not to square(root).js
+++ JavaScript/To square(root) or not to square(root).js
@@ -1,6 +1,4 @@
-Description:
-
-To square(root) or not to square(root)
+/*To square(root) or not to square(root)
 
 Write a method, that will get an integer array as parameter and will process every number from this array.
 Return a new array with processing every number of the input-array like this:
@@ -10,7 +8,7 @@ If the number has an integer square root, take this, otherwise square the number
 [4,3,9,7,2,1] -> [2,9,3,49,4,1]
 The input array will always contain only positive numbers and will never be empty or null.
 
-The input array should not be modified!
+The input array should not be modified!*/
 
 
 function squareOrSquareRoot(array) {
@@ -24,3 +22,37 @@ var out=[];
    }
   return out;  
 }
+
+
+const squareOrSquareRoot = array => array.map(a => Math.sqrt(a) % 1 == 0 ? Math.sqrt(a) : (a * a));
+
+function squareOrSquareRoot(array) {
+  return array.map(x => {
+    const r = Math.sqrt(x);
+    return (r % 1 == 0) ? r : (x*x);
+  });  
+}
+
+
+function squareOrSquareRoot(array) {
+  return array.map(a => 
+  {
+    var sqrt = Math.sqrt(a);
+    if(sqrt == Math.floor(sqrt))
+      {
+        return sqrt;
+      }
+      return a * a;
+  });  
+}
+
+const isInteger = (num) => (~~num === num);
+const squareOrSquareRoot = (array) => {
+  return array.map((num) => {
+    const sqrt = Math.sqrt(num);
+    return isInteger(sqrt) ? sqrt : (num * num);
+  });
+}
+
+
+squareOrSquareRoot = array => array.map(f = f => /\./.test(String(s = Math.sqrt(f))) ? Math.pow(f, 2) : s)
